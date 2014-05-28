d2ca93078289bf21092beefcf07651af64752978
kud1ing <github@kudling.de>
2014-05-28 21:24:23 +0200

    stub

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,13 +8,32 @@ import com.hp.hpl.jena.rdf.model.RDFNode
 import com.hp.hpl.jena.rdf.model.Resource
 import com.hp.hpl.jena.rdf.model.Statement
 import com.hp.hpl.jena.rdf.model.StmtIterator
+import java.io._
 import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
+    // TODO: Get the RDF files from somewhere.
+    val rdf_file_input_stream = new FileInputStream("/Users/lenny/Code/GitHub/Rdf/public.ttl")
+
+    // Create the model and load the RDF files.
+    val model = ModelFactory.createDefaultModel()
+    model.read(rdf_file_input_stream, null, "TTL")
+
+    def index = Action {
+        val string_builder = new StringBuilder()
+
+        val objects = model.listObjects()
+
+        // Iterate over all RDF objects.
+        while ( objects.hasNext() ) {
+            val o : RDFNode = objects.next();
+            string_builder ++= o.toString()
+        }
+
+        //Ok(views.html.index(string_builder.toString()))
+        Ok(string_builder.toString())
+    }
 
 }
