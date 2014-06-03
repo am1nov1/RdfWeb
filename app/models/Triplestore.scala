7604f952c90af5fb2901f5d0f4c49df38491f6ac
kud1ing <github@kudling.de>
2014-06-03 20:17:46 +0200

    make the file configurable

--- app/models/Triplestore.scala
+++ app/models/Triplestore.scala
@@ -6,11 +6,12 @@ import com.hp.hpl.jena.rdf.model.ModelFactory
 import com.hp.hpl.jena.rdf.model.Property
 import com.hp.hpl.jena.rdf.model.RDFNode
 import com.hp.hpl.jena.rdf.model.Resource
-import com.hp.hpl.jena.rdf.model.Selector;
-import com.hp.hpl.jena.rdf.model.SimpleSelector;
+import com.hp.hpl.jena.rdf.model.Selector
+import com.hp.hpl.jena.rdf.model.SimpleSelector
 import com.hp.hpl.jena.rdf.model.Statement
 import com.hp.hpl.jena.rdf.model.StmtIterator
 import java.io._
+import play.api.Play.current
 import scala.collection.JavaConversions._
 import scala.collection.mutable.HashMap
 import scala.collection.mutable.MutableList
@@ -48,8 +49,10 @@ object Triplestore {
     val SKOS_PREFLABEL: Property = model.createProperty(SKOS
         + "prefLabel")
 
-    // TODO: Get the RDF files from somewhere. Assets?
-    val rdf_file_input_stream = new FileInputStream("/Users/lenny/Code/GitHub/Rdf/public.ttl")
+    // TODO: handle the case when the parameter is not defined.
+    val rdf_file_name = current.configuration.getString("rdfWeb.rdfFiles").getOrElse("")
+
+    val rdf_file_input_stream = new FileInputStream(rdf_file_name)
 
     // Read the RDF files.
     model.read(rdf_file_input_stream, null, "TTL")
