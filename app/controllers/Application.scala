001fcd2ff35332afea6a9e339cbd0b6a15348a89
kud1ing <github@kudling.de>
2014-06-18 21:30:50 +0200

    rename, add controller

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,28 +10,33 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-    def predicates_html = TODO
+    def predicates = TODO
 
-    def index_html = Action {
+    def index = Action {
 
         Ok(views.html.index())
     }
 
-    def objects_html = Action {
+    def objects = Action {
 
         val objects = Triplestore.objects()
 
         Ok(views.html.objects(objects))
     }
 
-    def subject_html(subject_id: String) = Action {
+    def save() = Action {
+        // TODO: save
+        Ok("Saved")
+    }
+
+    def subject(subject_id: String) = Action {
 
         val subject = Triplestore.subject_details(subject_id)
 
         Ok(views.html.subject(subject))
     }
 
-    def subjects_html = Action {
+    def subjects = Action {
 
         val subjects = Triplestore.subjects()
 
---
+++ app/controllers/Statement.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import java.io._
+import models.RdfSubjectDetails
+import models.RdfTriple
+import models.Triplestore
+import play.api._
+import play.api.libs.json._
+import play.api.mvc._
+
+object Statement extends Controller {
+
+}
