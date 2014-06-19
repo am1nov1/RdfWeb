932f3370d45319f76d5f2db48a000d607d78ebf4
kud1ing <github@kudling.de>
2014-06-19 14:24:44 +0200

    reconfigure, controller stub

--- app/controllers/Statement.scala
+++ app/controllers/Statement.scala
@@ -8,6 +8,24 @@ import play.api._
 import play.api.libs.json._
 import play.api.mvc._
 
+/**
+ * A controller for RDF statements (triples).
+ */
 object Statement extends Controller {
 
+    /**
+     * Creates an RDF statement from the given JSON data.
+     */
+    /*
+    def create() = Action(parse.json) { request =>
+        Async {
+            val rdf_subject = request.body.\("s")
+            val rdf_predicate = request.body.\("p")
+            val rdf_object = request.body.\("o")
+
+            // TODO: return a proper JSON result
+            Ok("Created")
+        }
+    }
+    */
 }
